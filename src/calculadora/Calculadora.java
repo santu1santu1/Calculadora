4bb3e3c14f6f575a31e756f2297473a33a146ef3
Santi <Santi@192.168.1.48>
2021-03-03 13:59:12 -0600

    paso 1 y 2

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Santi
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        private String operador1;
+        private String operador2;
+        private String operacion;
+    }
+    
+}
