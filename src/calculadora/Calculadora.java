ecded00ae6b71f9c43cd62967028c8d912e126d5
Santi <Santi@192.168.1.48>
2021-03-03 14:03:59 -0600

    paso 3 y 4

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -11,13 +11,35 @@ package calculadora;
  */
 public class Calculadora {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
         private String operador1;
         private String operador2;
         private String operacion;
+
+    public String getOperador1() {
+        return operador1;
     }
+
+    public void setOperador1(String operador1) {
+        this.operador1 = operador1;
+    }
+
+    public String getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(String operador2) {
+        this.operador2 = operador2;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+        
+        
     
 }
